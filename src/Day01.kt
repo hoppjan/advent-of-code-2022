f40aafb0f2c3e21ffa273b201fb88c76f26f7ee2
hoppjan <83405332+hoppjan@users.noreply.github.com>
2022-12-02 11:48:48 +0100

    Solved day #01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val testLines = readInput("Day01_test")
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val testResult1 = part1(testLines)
+    check(testResult1 == 24_000)
+    println("test part 1: $testResult1")
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testResult2 = part2(testLines)
+    check(testResult2 == 45_000)
+    println("test part 2: $testResult2")
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    val lines = readInput("Day01")
+
+    println("part 1: ${part1(lines)}")
+    println("part 2: ${part2(lines)}")
 }
+
+private fun part1(input: List<String>) = parseCalories(input)
+    .maxOf { list ->
+        list.sumOf { it.toInt() }
+    }
+
+private fun part2(input: List<String>) = parseCalories(input)
+    .map { list -> list.sumOf { it.toInt() } }
+    .sorted()
+    .takeLast(3)
+    .sumOf { it }
+
+private fun parseCalories(input: List<String>) =
+    input.joinToString(separator = "\n")
+        .trim()
+        .split("\n\n")
+        .map { lines -> lines.split("\n").filter { it.isNotBlank() } }
